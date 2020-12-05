70277d0f0ee44de9c6ac93124de6ac2c1daad2d7
FractalArt <FractalArt@users.noreply.github.com>
2020-12-05 18:31:04 +0100

    Slight improvement to day 3 test.

--- tests/day_3.rs
+++ tests/day_3.rs
@@ -6,6 +6,6 @@ fn test_day_3() {
 
     let task_1 = aoc2020::day_3::task_1(&data, 3, 1);
     assert_eq!(task_1, 156);
-    let task_2 = aoc2020::day_3::task_2(&data, &vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]);
+    let task_2 = aoc2020::day_3::task_2(&data, &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]);
     assert_eq!(task_2, 3521829480);
 }
